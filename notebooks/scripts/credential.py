bfb07582332e53b0129374ec6ac4055c5c7b2871
Rodrigo Brust <53950449+rodreras@users.noreply.github.com>
2025-02-21 11:37:45 +0000

    Update credential.py

--- notebooks/scripts/credential.py
+++ notebooks/scripts/credential.py
@@ -1,4 +1,4 @@
-username_landsat = 'rodrigobrusts@gmail.com'
-password_landsat = '@Rodrigo_2023'
-username_sentinel = 'rodrigobrust'
-password_sentinel = 'mA9cM0As#D'
+username_landsat = 'HIDDEN'
+password_landsat = 'HIDDEN'
+username_sentinel = 'HIDDEN'
+password_sentinel = 'HIDDEN'
